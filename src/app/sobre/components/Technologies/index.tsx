be03eec13231eac8dfbfb572b113804a6e4d43fa
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-03-01 11:54:37 -0300

    style: added gray images

---
+++ src/app/sobre/components/Technologies/index.tsx
@@ -0,0 +1,39 @@
+import { Title } from '@/components/Title'
+import { TechnologyGroup } from '@/types/technologies'
+import { technologiesGroups } from '@/utils/technologies'
+import Image from 'next/image'
+import React from 'react'
+
+export const Technologies = () => {
+  return (
+    <div className='flex flex-col lg:flex-row flex-wrap gap-8 w-full'>
+      {technologiesGroups.map((technologyGroup:TechnologyGroup) => (
+        <div key={technologyGroup.label} className='lg:w-[40%] w-full'>
+          <Title tag='h3' className='mb-4'>{technologyGroup.label}</Title>
+          <div className='flex gap-4'>
+            {technologyGroup.technologies.map((technology) => (
+              <div key={technology.name} className='w-[60px] h-[60px] relative bg-gray-800 p-1 group'>
+                {/* Imagem em preto e branco (padrão) */}
+                <Image 
+                  src={technology.grayImage} 
+                  alt={technology.name} 
+                  className='absolute inset-0 w-full h-full transition-opacity duration-300'
+                />
+                {/* Imagem colorida (aparece no hover) */}
+                <Image 
+                  src={technology.coloredImage} 
+                  alt={technology.name} 
+                  className='absolute inset-0 w-full h-full opacity-0 transition-opacity duration-300 group-hover:opacity-100'
+                />
+                {/* Tooltip (Nome da tecnologia) */}
+                <span className="absolute left-1/2 -bottom-8 transform -translate-x-1/2 bg-gray-900 text-white text-xs px-2 py-1 rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  {technology.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/utils/technologies.tsx
+++ src/utils/technologies.tsx
@@ -1,53 +1,71 @@
 import java from '@/assets/technologyImages/back-end/java.png'
 import nodeJS from '@/assets/technologyImages/back-end/nodejs.png'
-import grayNodeJS from '@/assets/technologyImages/back-end/grayImage/nodeJS-gray.png'
 import php from '@/assets/technologyImages/back-end/php.png'
+
+import grayJava from '@/assets/technologyImages/back-end/grayImages/java-gray.png'
+import grayNodeJS from '@/assets/technologyImages/back-end/grayImages/nodeJS-gray.png'
+import grayPhp from '@/assets/technologyImages/back-end/grayImages/php-gray.png'
+
+
 
 import nextJS from '@/assets/technologyImages/front-end/nextjs.png'
 import reactJS from '@/assets/technologyImages/front-end/reactjs.png'
 import tailwindcss from '@/assets/technologyImages/front-end/tailwindcss.png'
 import typescript from '@/assets/technologyImages/front-end/typescript.png'
+
+import grayNextJS from '@/assets/technologyImages/front-end/grayImages/nextjs-gray.png'
+import grayReactJS from '@/assets/technologyImages/front-end/grayImages/reactjs-gray.png'
+import grayTailwindcss from '@/assets/technologyImages/front-end/grayImages/tailwindcss-gray.png'
 import typescriptGray from '@/assets/technologyImages/front-end/grayImages/typescript-gray.png'
 
 import mysql from '@/assets/technologyImages/database/mysql.png'
 import supabase from '@/assets/technologyImages/database/supabase.png'
 
+import grayMysql from '@/assets/technologyImages/database/grayImages/mysql-gray.png'
+import graySupabase from '@/assets/technologyImages/database/grayImages/supabase-gray.png'
+
 import git from '@/assets/technologyImages/versioning/git.png'
 import github from '@/assets/technologyImages/versioning/github.png'
 import wordpress from '@/assets/technologyImages/crm/wordpress.png'
+
+import grayGit from '@/assets/technologyImages/versioning/grayImages/git-gray.png'
+import grayGithub from '@/assets/technologyImages/versioning/grayImages/github-gray.png'
+import grayWordpress from '@/assets/technologyImages/crm/grayImages/wordpress-gray.png'
+
+
 import { TechnologyGroup } from '@/types/technologies'
 
 export const technologiesGroups: TechnologyGroup[] = [
   {
     label: "Back-end",
     technologies: [
-      { name: "Java", grayImage: java, coloredImage: java },
+      { name: "Java", grayImage: grayJava, coloredImage: java },
       { name: "Node.js", grayImage: grayNodeJS, coloredImage: nodeJS },
-      { name: "PHP", grayImage: php, coloredImage: php }
+      { name: "PHP", grayImage: grayPhp, coloredImage: php }
     ]
   },
   {
     label: "Front-end",
     technologies: [
-      { name: "Next.js", grayImage: nextJS, coloredImage: nextJS },
-      { name: "React.js", grayImage: reactJS, coloredImage: reactJS },
-      { name: "Tailwind CSS", grayImage: tailwindcss, coloredImage: tailwindcss },
+      { name: "Next.js", grayImage: grayNextJS, coloredImage: nextJS },
+      { name: "React.js", grayImage: grayReactJS, coloredImage: reactJS },
+      { name: "Tailwind CSS", grayImage: grayTailwindcss, coloredImage: tailwindcss },
       { name: "TypeScript", grayImage: typescriptGray, coloredImage: typescript }
     ]
   },
   {
     label: "Banco de dados",
     technologies: [
-      { name: "MySQL", grayImage: mysql, coloredImage: mysql },
-      { name: "Supabase", grayImage: supabase, coloredImage: supabase }
+      { name: "MySQL", grayImage: grayMysql, coloredImage: mysql },
+      { name: "Supabase", grayImage: graySupabase, coloredImage: supabase }
     ]
   },
   {
     label: "Ferramentas",
     technologies: [
-      { name: "Git", grayImage: git, coloredImage: git },
-      { name: "GitHub", grayImage: github, coloredImage: github },
-      { name: "WordPress", grayImage: wordpress, coloredImage: wordpress }
+      { name: "Git", grayImage: grayGit, coloredImage: git },
+      { name: "GitHub", grayImage: grayGithub, coloredImage: github },
+      { name: "WordPress", grayImage: grayWordpress, coloredImage: wordpress }
     ]
   }
 ];
