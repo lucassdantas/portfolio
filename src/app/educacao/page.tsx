0267977b220d1a4d44057379fc3a9ea0d605ade2
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-03-02 21:02:12 -0300

    strcuture: begin education and projects page

--- src/app/educacao/page.tsx
+++ src/app/educacao/page.tsx
@@ -1,7 +1,16 @@
+import { Section } from '@/components/Section'
+import { Title } from '@/components/Title'
 import React from 'react'
 
 export default function Education () {
   return (
-    <div>Education</div>
+    <Section limiterClassname='relative  h-full' className=' text-base_1-a0'>
+      <div className='mb-24'>
+        <Title tag='h1' className='font-extrabold'>Educação</Title>
+        <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at convallis turpis. Proin sit amet eros a justo malesuada facilisis. Integer auctor, ex a vestibulum hendrerit, felis purus facilisis velit, eu luctus.</p>
+      </div>
+      <div>
+      </div>
+    </Section>
   )
 }
--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,7 +1,16 @@
+import { Section } from '@/components/Section'
+import { Title } from '@/components/Title'
 import React from 'react'
 
 export default function Projects () {
   return (
-    <div>Projects</div>
+    <Section limiterClassname='relative  h-full' className=' text-base_1-a0'>
+      <div className='mb-24'>
+        <Title tag='h1' className='font-extrabold'>Projetos</Title>
+        <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at convallis turpis. Proin sit amet eros a justo malesuada facilisis. Integer auctor, ex a vestibulum hendrerit, felis purus facilisis velit, eu luctus.</p>
+      </div>
+      <div>
+      </div>
+    </Section>
   )
 }
