77be3356b3d454ac4bebe28bfe868ab1d3ae5ad8
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-03-10 12:30:56 -0300

    data: adding projects data

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,27 +1,29 @@
 import { Project } from "@/types/projectType";
 import { javaTechnology, tailwindTechnology } from "@/utils/technologies";
 
+
+import eletronicUrn from '@/assets/Projects/urna-eletronica/urna-eletronica.png'
 import portfolio from '@/assets/Projects/portfolio.png'
 import education from '@/assets/Projects/educacao.png'
 
 export const projects:Project[] = [
   {
-    name:'projeto 1',
-    description:'projeto',
-    mainImage:portfolio,
-    galleryImages:[education,portfolio],
-    repositoryUrl:'',
-    liveProjectUrl:'aaaaaaaaaaaaaaaaaaaaaa',
-    category:'Wordpress',
+    name:'Portfólio 1',
+    description:'Meu portfólio',
+    mainImage:eletronicUrn,
+    galleryImages:[eletronicUrn],
+    repositoryUrl:'https://github.com/lucassdantas/portfolio',
+    liveProjectUrl:'https://devdantas.com.br/',
+    category:'Sites',
     technologies:[javaTechnology,]
   },
   {
-    name:'projeto 2',
-    description:'projeto 2',
+    name:'Urna Eletrônica',
+    description:'Urna Eletrônica feita com Javascript focado em orientar a modulos',
     mainImage:education,
     galleryImages:[portfolio,education],
     repositoryUrl:'aaaaaaaaaa',
-    liveProjectUrl:'',
+    liveProjectUrl:'https://lucassdantas.github.io/softwares/Urna/index.html',
     category:'Sites',
     technologies:[tailwindTechnology,]
   },
