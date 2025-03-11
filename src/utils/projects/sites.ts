8604a540c2b67a3ea9d657644c17ef1c9ad6668b
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-03-11 09:52:10 -0300

    data: added more projects

--- src/utils/projects/sites.ts
+++ src/utils/projects/sites.ts
@@ -1,34 +1,100 @@
 import { Project } from "@/types/projectType";
-import { githubTechnology, gitTechnology, javaTechnology, reactJSTechnology, tailwindTechnology, typescriptTechnology, wordpressTechnology } from "@/utils/technologies";
+import { githubTechnology, gitTechnology, javaTechnology, nextJSTechnology, nodeJSTechnology, phpTechnology, reactJSTechnology, tailwindTechnology, typescriptTechnology, wordpressTechnology } from "@/utils/technologies";
 
-import autovistoria from '@/assets/Projects/sites/wordpress/autovistoria-predial-focon/autovistoria-predial-focon-main.jpg'
-import autovistoria2 from '@/assets/Projects/sites/wordpress/autovistoria-predial-focon/autovistoria-predial-focon-form.jpg'
-import autovistoria3 from '@/assets/Projects/sites/wordpress/autovistoria-predial-focon/autovistoria-predial-focon-itens.jpg'
-
-import seutreino from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-main.jpg'
-import seutreino2 from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-sobre.jpg'
-import seutreino3 from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-calculadora.jpg'
+import seutreino28dias from '@/assets/Projects/sites/seu-treino-28-dias/seu-treino-28-dias-main.jpg'
+import seutreino28dias2 from '@/assets/Projects/sites/seu-treino-28-dias/seu-treino-28-dias-sessao.jpg'
+import seutreino28dias3 from '@/assets/Projects/sites/seu-treino-28-dias/seu-treino-28-dias-sobre.jpg'
 export const sites:Project[] = [
   
   {
-    name:'Seu Treino',
-    description:'Site de personal trainer feito com WP, Elementor, WooCommerce e Jet Engine',
-    mainImage:seutreino,
-    galleryImages:[seutreino2, seutreino3],
+    name:'Postfast',
+    description:'Landing page feita com Next e Tailwind. Configurei hospedagem em VPS',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
     repositoryUrl:'',
-    liveProjectUrl:'https://seutreino.com/',
-    category:'Wordpress',
-    technologies:[wordpressTechnology]
+    liveProjectUrl:'https://www.postfast.com.br/',
+    category:'Site',
+    technologies:[nextJSTechnology, tailwindTechnology, nodeJSTechnology, gitTechnology, githubTechnology]
   },
   {
-    name:'Autovistoria Predial',
-    description:'Site de autovistoria do qual participei do desenvolvimento - feito com WP e Elementor',
-    mainImage:autovistoria,
-    galleryImages:[autovistoria2, autovistoria3],
+    name:'Seu Treino - 28 dias para mudar',
+    description:'Landing page feita com React e Tailwind',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
+    repositoryUrl:'https://github.com/lucassdantas/seu-treino-28-dias-lp',
+    liveProjectUrl:'https://seutreino.com/28-dias-para-mudar/',
+    category:'Site',
+    technologies:[reactJSTechnology, tailwindTechnology]
+  },
+  {
+    name:'Thialita',
+    description:'Landing page feita com Next e Tailwind, formulários com formJS',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
     repositoryUrl:'',
-    liveProjectUrl:'https://autovistoriapredial.eng.br/',
-    category:'Wordpress',
-    technologies:[wordpressTechnology]
+    liveProjectUrl:'https://www.thialitasilva.com.br/',
+    category:'Site',
+    technologies:[nextJSTechnology, tailwindTechnology]
+  },
+  {
+    name:'Diagonal Ag',
+    description:'Site onepage para agência Diagonal',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
+    repositoryUrl:'https://github.com/lucassdantas/diagonal-ag',
+    liveProjectUrl:'https://diagonal.ag/',
+    category:'Site',
+    technologies:[reactJSTechnology, tailwindTechnology, phpTechnology, gitTechnology, githubTechnology]
+  },
+  {
+    name:'Revista Ibremp',
+    description:'Site institucional para apresentação das revistas Ibremp',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
+    repositoryUrl:'https://github.com/lucassdantas/rbdsre-revista-ibremp',
+    liveProjectUrl:'https://rbdsre.ibremp.org.br/',
+    category:'Site',
+    technologies:[nextJSTechnology, tailwindTechnology, gitTechnology, githubTechnology]
+  },
+  {
+    name:'Corpore',
+    description:'Site onepage para a clínica de fisioterapia Corpore',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
+    repositoryUrl:'https://github.com/lucassdantas/corporetorre',
+    liveProjectUrl:'https://www.corporetorre.com.br/',
+    category:'Site',
+    technologies:[reactJSTechnology, tailwindTechnology, phpTechnology, gitTechnology, githubTechnology]
+  },
+  {
+    name:'Carneiro e Carballido',
+    description:'Site instutucional para empresa de contabilidade',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
+    repositoryUrl:'https://github.com/lucassdantas/Carneiro-e-Carballido',
+    liveProjectUrl:'https://cecconsultores.com.br/',
+    category:'Site',
+    technologies:[reactJSTechnology, tailwindTechnology, phpTechnology, gitTechnology, githubTechnology]
+  },
+  {
+    name:'Carlos Psicanalista',
+    description:'LP para psicanalista',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
+    repositoryUrl:'https://github.com/lucassdantas/carlos-psicanalista-nextjs',
+    liveProjectUrl:'https://www.carlosalbertopsicanalista.com.br/',
+    category:'Site',
+    technologies:[nextJSTechnology, tailwindTechnology, gitTechnology, githubTechnology]
+  },
+  {
+    name:'Cópia de Minha Biblioteca Católica',
+    description:'Cópia do site minha biblioteca católica',
+    mainImage:seutreino28dias,
+    galleryImages:[seutreino28dias2, seutreino28dias3],
+    repositoryUrl:'https://github.com/lucassdantas/lucassdantas.github.io/tree/main/sites/catolica',
+    liveProjectUrl:'https://lucassdantas.github.io/sites/catolica/',
+    category:'Site',
+    technologies:[reactJSTechnology, tailwindTechnology]
   },
  
 ]
---
+++ src/utils/projects/wordpress.ts
@@ -0,0 +1,44 @@
+import { Project } from "@/types/projectType";
+import { githubTechnology, gitTechnology, javaTechnology, reactJSTechnology, tailwindTechnology, typescriptTechnology, wordpressTechnology } from "@/utils/technologies";
+
+import autovistoria from '@/assets/Projects/sites/wordpress/autovistoria-predial-focon/autovistoria-predial-focon-main.jpg'
+import autovistoria2 from '@/assets/Projects/sites/wordpress/autovistoria-predial-focon/autovistoria-predial-focon-form.jpg'
+import autovistoria3 from '@/assets/Projects/sites/wordpress/autovistoria-predial-focon/autovistoria-predial-focon-itens.jpg'
+
+import seutreino from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-main.jpg'
+import seutreino2 from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-sobre.jpg'
+import seutreino3 from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-calculadora.jpg'
+export const sites:Project[] = [
+  
+  {
+    name:'Seu Treino',
+    description:'Site de personal trainer feito com WP, Elementor, WooCommerce e Jet Engine',
+    mainImage:seutreino,
+    galleryImages:[seutreino2, seutreino3],
+    repositoryUrl:'',
+    liveProjectUrl:'https://seutreino.com/',
+    category:'Wordpress',
+    technologies:[wordpressTechnology]
+  },
+  {
+    name:'Autovistoria Predial',
+    description:'Site de autovistoria do qual participei do desenvolvimento - feito com WP e Elementor',
+    mainImage:autovistoria,
+    galleryImages:[autovistoria2, autovistoria3],
+    repositoryUrl:'',
+    liveProjectUrl:'https://autovistoriapredial.eng.br/',
+    category:'Wordpress',
+    technologies:[wordpressTechnology]
+  },
+  {
+    name:'Seu Treino - 28 dias para mudar',
+    description:'Landing page feita com React e Tailwind',
+    mainImage:autovistoria,
+    galleryImages:[autovistoria2, autovistoria3],
+    repositoryUrl:'',
+    liveProjectUrl:'https://seutreino.com/28-dias-para-mudar/',
+    category:'Wordpress',
+    technologies:[reactJSTechnology, tailwindTechnology]
+  },
+ 
+]
