1bdcc5af104574f98fb9cfe6bad7ba94d6fe1e30
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-03-11 08:04:35 -0300

    adding more projects

--- src/utils/projects/sites.ts
+++ src/utils/projects/sites.ts
@@ -5,11 +5,24 @@ import autovistoria from '@/assets/Projects/sites/wordpress/autovistoria-predial
 import autovistoria2 from '@/assets/Projects/sites/wordpress/autovistoria-predial-focon/autovistoria-predial-focon-form.jpg'
 import autovistoria3 from '@/assets/Projects/sites/wordpress/autovistoria-predial-focon/autovistoria-predial-focon-itens.jpg'
 
+import seutreino from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-main.jpg'
+import seutreino2 from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-sobre.jpg'
+import seutreino3 from '@/assets/Projects/sites/wordpress/seu-treino/seu-treino-calculadora.jpg'
 export const sites:Project[] = [
   
   {
+    name:'Seu Treino',
+    description:'Site de personal trainer feito com WP, Elementor, WooCommerce e Jet Engine',
+    mainImage:seutreino,
+    galleryImages:[seutreino2, seutreino3],
+    repositoryUrl:'',
+    liveProjectUrl:'https://seutreino.com/',
+    category:'Wordpress',
+    technologies:[wordpressTechnology]
+  },
+  {
     name:'Autovistoria Predial',
-    description:'Site de autovistoria feito com WP e Elementor',
+    description:'Site de autovistoria do qual participei do desenvolvimento - feito com WP e Elementor',
     mainImage:autovistoria,
     galleryImages:[autovistoria2, autovistoria3],
     repositoryUrl:'',
