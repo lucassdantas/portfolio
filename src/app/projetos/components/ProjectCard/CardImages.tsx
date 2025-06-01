29bcd3ec3cff8617f13776e21bba0accbcfb411c
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-06-01 13:33:31 -0300

    add 100 width on main project image if galleryImages is empty

--- src/app/projetos/components/ProjectCard/CardImages.tsx
+++ src/app/projetos/components/ProjectCard/CardImages.tsx
@@ -11,30 +11,32 @@ interface CardImagesProps {
 export const CardImages = ({ project, handleImageClick, setIsLightboxOpen }: CardImagesProps) => {
   return (
     <div className="flex w-full justify-between gap-4">
-      <div className="lg:w-[300px] w-full">
+      <div className={`${project.galleryImages.length>0 && 'lg:w-[300px]'} w-full`}>
         <Image
           src={project.mainImage}
           alt={project.name}
           width={300}
           height={200}
           onClick={() => { handleImageClick(0); setIsLightboxOpen(true) }} // Clica para abrir a lightbox
-          className="cursor-pointer lg:w-[300px] w-full h-full lg:h-[200px] object-cover"
+          className={`cursor-pointer ${project.galleryImages.length>0 && 'lg:w-[300px]'} w-full h-full lg:h-[200px] object-cover`}
         />
       </div>
 
-      <div className="hidden lg:flex flex-col gap-2 h-[170px] overflow-y-scroll scrollbar-visible">
-        {project.galleryImages.map((image, index) => (
-          <Image
-            key={index}
-            src={image}
-            alt={`${project.name} gallery`}
-            width={120}
-            height={80}
-            onClick={() => { handleImageClick(index + 1); setIsLightboxOpen(true) }} // Clica para abrir a lightbox
-            className="cursor-pointer w-full h-[80px] object-cover"
-          />
-        ))}
-      </div>
+      {project.galleryImages && 
+        <div className="hidden lg:flex flex-col gap-2 h-[170px] overflow-y-scroll scrollbar-visible">
+          {project.galleryImages.map((image, index) => (
+            <Image
+              key={index}
+              src={image}
+              alt={`${project.name} gallery`}
+              width={120}
+              height={80}
+              onClick={() => { handleImageClick(index + 1); setIsLightboxOpen(true) }} // Clica para abrir a lightbox
+              className="cursor-pointer w-full h-[80px] object-cover"
+            />
+          ))}
+        </div>
+      }
     </div>
   )
 }
